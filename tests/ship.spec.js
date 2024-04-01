8aadce000076105d5ba43ea0ef89336ac5ecf0b0
Drake Williamson <williamson.drake@outlook.com>
2024-03-31 17:46:10 -0700

    feat: write test for existence of ship constructor

--- tests/ship.spec.js
+++ tests/ship.spec.js
@@ -1,7 +1,11 @@
-import ships from '../src/logic/ships';
+import Ship from '../src/logic/ships';
 
 describe('test ship class functionality', () => {
   test('should exist', () => {
-    expect(ships).toBeDefined();
+    expect(Ship).toBeDefined();
+  });
+  test('ships class exists', () => {
+    const newShip = new Ship('test ship');
+    expect(newShip).toBeDefined();
   });
 });
