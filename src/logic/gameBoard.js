70a1d1cee74b75f166d5940996ac6e1eae336862
Drake Williamson <williamson.drake@outlook.com>
2024-04-02 19:02:11 -0700

    feat: create and pass gameboard 2 dimensional array

--- src/logic/gameBoard.js
+++ src/logic/gameBoard.js
@@ -1,5 +1,14 @@
 export default class Gameboard {
   constructor(name) {
     this.name = name;
+    this.boardArr = [];
+    (() => {
+      const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+      for (let i = 0; i < 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+          this.boardArr.push(`${letters[i]}${j}`);
+        }
+      }
+    })();
   }
 }
