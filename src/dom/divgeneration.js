136a2ccefaa8a737281dfaeb75129ede54afa9ab
Drake Williamson <williamson.drake@outlook.com>
2024-04-08 22:40:50 -0700

    feat: create divgeneration function

---
+++ src/dom/divgeneration.js
@@ -0,0 +1,20 @@
+const playerBoard = document.querySelector('.player-board');
+const computerBoard = document.querySelector('.computer-board');
+
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+export default function generateDivs(boardName) {
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 1; j <= 10; j += 1) {
+      const newDiv = document.createElement('div');
+      newDiv.setAttribute('coordinate', `${letters[i]}${j}`);
+      newDiv.setAttribute('boardName', boardName);
+      console.log(newDiv);
+      if (boardName === 'player') {
+        playerBoard.appendChild(newDiv);
+      } else {
+        computerBoard.appendChild(newDiv);
+      }
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import css from './style.css';
+import generateDivs from './dom/divgeneration';
+
+generateDivs('player');
+generateDivs('computer');
